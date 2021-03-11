a8393e44680c25184bc513e3a4b63d61e203bf1f
LudovickL <Luberisselud@gmail.com>
2021-03-11 14:50:44 -0500

    ajout sur github

---
+++ README.md
@@ -0,0 +1,4 @@
+# Application Signets
+
+Application utilisée dans le cadre des cours 582-640 et 582-4PA pour apprendre les notions de base du développement
+d'applications Web monopages réactives (Cadriciels/Plateformes utilisés : React/Firebase).
---
+++ src/composants/Appli.jsx
@@ -0,0 +1,31 @@
+import './Appli.scss';
+import Entete from './Entete';
+import ListeDossiers from './ListeDossiers';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Accueil from './Accueil';
+import { useState } from 'react';
+
+export default function Appli() {
+  const etatUtilisateur = useState(null);
+  const [utilisateur, setUtilisateur] = etatUtilisateur;
+
+  return (
+    <div className="Appli">
+      {
+        utilisateur ?
+          <>
+            <Entete />
+            <section className="contenu-principal">
+              <ListeDossiers />
+              <Fab className="ajoutRessource" color="primary" aria-label="Ajouter dossier">
+                <AddIcon />
+              </Fab>
+            </section>
+          </>
+        :
+          <Accueil />
+      }
+    </div>
+  );
+}
